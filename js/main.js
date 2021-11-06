afdd4518dedb8549f5b79246d066e91b34ff6e35
NOISE <46085246+rcy1314@users.noreply.github.com>
2021-11-07 02:20:06 +0800

    添加随机全屏背景壁纸

--- js/main.js
+++ js/main.js
@@ -119,38 +119,6 @@ function time() {
     t = setTimeout(time, 1000);
 }
 
-//必应壁纸
-var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
-var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
-var index = sessionStorage.getItem("index");
-var $section = $('#section');
-if (imgUrls == null) {
-    imgUrls = new Array();
-    index = 0;
-    $.get(url, function (result) {
-        images = result.images;
-        for (let i = 0; i < images.length; i++) {
-            const item = images[i];
-            imgUrls.push(item.url);
-        }
-        var imgUrl = imgUrls[index];
-        var url = "https://www.bing.com" + imgUrl;
-        $section.css("background", "url('" + url + "') center center no-repeat #666");
-        $section.css("background-size", "cover");
-        sessionStorage.setItem("imgUrls", JSON.stringify(imgUrls));
-        sessionStorage.setItem("index", index);
-    });
-} else {
-    if (index == 7)
-        index = 0;
-    else
-        index++;
-    var imgUrl = imgUrls[index];
-    var url = "https://www.bing.com" + imgUrl;
-    $section.css("background", "url('" + url + "') center center no-repeat #666");
-    $section.css("background-size", "cover");
-    sessionStorage.setItem("index", index);
-}
 
 //加载动画
 window.addEventListener('load', function () {
