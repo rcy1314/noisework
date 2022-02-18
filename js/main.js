2e1445c16f5532dcce5f160408678909de5f83b5
NOISE <46085246+rcy1314@users.noreply.github.com>
2022-02-18 23:06:23 +0800

    更改网址

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ iziToast.settings({
 iziToast.info({
     icon: 'fad fa-vial',
     title: '欢迎访问我的主页,这里介绍了我的其它站点链接，可点击访问它们',
-    message: '主页：https://noisework.cn，备用地址1:https://noisework.pages.dev，备用地址2:https://noisework.vercel.app'
+    message: '主页：https://noisework.cn，备用地址1:https://noisework.pages.dev，备用地址2：https://noisework-cn.vercel.app，备用地址3:https://noisework.vercel.app'
 });
 
 //控制台输出
