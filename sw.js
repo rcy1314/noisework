7da4a0d12d7ccdcd7ed53ea6b475b78300a2a3cd
NOISE <46085246+rcy1314@users.noreply.github.com>
2022-12-04 03:38:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-navigator.serviceWorker&&navigator.serviceWorker.register("/sw.js?v=20221203101045").then((function(){navigator.serviceWorker.addEventListener("message",(function(e){if("sw.update"===e.data){let e=document.querySelector("meta[name=theme-color]"),s=document.createElement("div");e&&(e.content="#000"),s.innerHTML='<div><link rel="stylesheet" href="https://cdn1.tianli0.top/gh/Colsrch/CDN/hexo-offline-popup.css"><div class="c-message animated animated-lento slideInRight"><i class=" c-message--icon c-message--success"></i><div class="el-notification__group"><h2 class="c-message__title">操作通知</h2><div class="el-notification__content">已更新最新版本（刷新生效）</div><div class="c-message--close" onclick="location.reload()">×</div></div></div></div>',document.body.appendChild(s),setTimeout((function(){document.getElementById("app-refresh").className+=" app-refresh-show"}),16)}}))}));
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
+);
+
+// This is your Service Worker, you can put any of your custom Service Worker
+// code in this file, above the `precacheAndRoute` line.
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
