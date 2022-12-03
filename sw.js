26df1bc1c92a15eb76d9d535bb5c1641010a7c58
NOISE <46085246+rcy1314@users.noreply.github.com>
2022-12-04 02:37:09 +0800

    1

--- sw.js
+++ sw.js
@@ -45,3 +45,21 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
