b96bf9d7eb93860ae778554250cda5f6572986cc
NOISE <46085246+rcy1314@users.noreply.github.com>
2022-12-04 08:21:01 +0800

    1

--- sw.js
+++ sw.js
@@ -1,8 +1,35 @@
 importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-// This is your Service Worker, you can put any of your custom Service Worker
-// code in this file, above the `precacheAndRoute` line.
+console.log( "workbox 6 | 20220823", workbox )
+const {registerRoute} = workbox.routing;
+const {NetworkFirst} = workbox.strategies;
+//  const {CacheableResponsePlugin} = workbox.cacheable.response;
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+workbox.LOG_LEVEL = "debug";
+//console.log("Syhper SW Begin | " + workbox.LOG_LEVEL );
+try {
+  clients.claim();
+console.log(" == == == sw | claimed --", navigator.serviceWorker.controller )
+} catch (error) {
+console.log(" == == == sw | not claiming --")
+}
+
+const cacheName = 'sampleCache';
+//  const matchCallback = ({request}) => request.mode === 'navigate';
+const matchCallback = ({request}) => {
+if(request.url.indexOf(".vue") != -1) console.log("---- sw req | ", request.url)
+  return true;
+};
+
+const networkTimeoutSeconds = 3;
+
+registerRoute(
+  matchCallback,
+  new NetworkFirst({
+    networkTimeoutSeconds,
+    "cacheName": "sampleCache",
+    "matchOptions": {"ingoreSearch": true}
+  })
+);
