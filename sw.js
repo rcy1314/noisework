0d938ca1a689fdaffc280442261bdc98f0266c22
NOISE <46085246+rcy1314@users.noreply.github.com>
2022-12-05 10:40:46 +0800

    更进

--- sw.js
+++ sw.js
@@ -1,15 +1,3 @@
-const cacheVersion = 'v1';
-const filesToCache = [
-  'favicon.ico',
-  'index.html',
-  './js/APlayer.min.js',
-  './js/index.js',
-  './js/Meting.min.js',
-  './js/instantclick.min.js',
-  './css/index.css',
-  './assets/favicon.png',
-];
-
 self.addEventListener('install', event => {
   console.log('[ServiceWorker] Install');
   event.waitUntil(
@@ -21,32 +9,51 @@ self.addEventListener('install', event => {
   );
 });
 
+const cacheVersion = 'v1';
+const filesToCache = [
+  './assets/favicon.png',
+  './js/APlayer.min.js',
+  './js/index.js',
+  './js/Meting.min.js',
+  './js/instantclick.min.js',
+  './css/index.css',
+  './assets/favicon.png',
+  './index.html',
+];
+
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
+
+
 self.addEventListener('activate', event => {
   console.log('[ServiceWorker] Activate');
 });
 
-self.addEventListener('fetch', event => {
-  console.log('[ServiceWorker] fetch', event.request);
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
-  );
-});
-const cacheVersion = 'v2';
-const filesToCache = [
-  ...
-];
-...
-self.addEventListener('activate', event => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil(
-    caches.keys()
-    .then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== cacheVersion) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }));
+  }));
 });
