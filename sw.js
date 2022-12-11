ebbfbadf189c098fe60b3704af38e456e17341ce
NOISE <46085246+rcy1314@users.noreply.github.com>
2022-12-12 00:12:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -57,3 +57,10 @@ self.addEventListener('activate', (e) => {
     }));
   }));
 });
+self.addEventListener('fetch', (fetchEvent) => {
+  fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then((res) => {
+          return res || fetch(fetchEvent.request);
+      })
+  );
+});
