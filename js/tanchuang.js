bdac01ac2437ac03a32feebe90b90af7ddffad38
NOISE <46085246+rcy1314@users.noreply.github.com>
2023-02-24 20:10:07 +0800

    更改样式

---
+++ js/tanchuang.js
@@ -0,0 +1,130 @@
+//加载完成后执行
+window.addEventListener('load', function () {
+    //载入动画
+    $('#loading-box').attr('class', 'loaded');
+    $('#bg').css("cssText", "transform: scale(1);filter: blur(0px);transition: ease 1.5s;");
+    $('#section').css("cssText", "opacity: 1;transition: ease 2.5s;");
+    $('.cover').css("cssText", "opacity: 1;transition: ease 2.5s;");
+
+    //用户欢迎
+    iziToast.settings({
+        timeout: 3000,
+        backgroundColor: '#ffffff40',
+        titleColor: '#efefef',
+        messageColor: '#efefef',
+        progressBar: false,
+        close: false,
+        closeOnEscape: true,
+        position: 'topCenter',
+        transitionIn: 'bounceInDown',
+        transitionOut: 'flipOutX',
+        displayMode: 'replace',
+        layout: '1'
+    });
+    setTimeout(function () {
+        iziToast.show({
+            title: hello,
+            message: '❤ 欢迎您访问 ❤ 请点击后进入'
+        });
+    }, 800);
+
+    //中文字体缓加载-此处写入字体源文件
+    //先行加载简体中文子集，后续补全字集
+    //由于压缩过后的中文字体仍旧过大，可转移至对象存储或 CDN 加载
+    const font = new FontFace(
+        "MiSans",
+        "url(" + "./font/MiSans-Regular.woff2" + ")"
+    );
+    document.fonts.add(font);
+
+}, false)
+
+//进入问候
+now = new Date(), hour = now.getHours()
+if (hour < 6) {
+    var hello = "凌晨好";
+} else if (hour < 9) {
+    var hello = "早上好";
+} else if (hour < 12) {
+    var hello = "上午好";
+} else if (hour < 14) {
+    var hello = "中午好";
+} else if (hour < 17) {
+    var hello = "下午好";
+} else if (hour < 19) {
+    var hello = "傍晚好";
+} else if (hour < 22) {
+    var hello = "晚上好";
+} else {
+    var hello = "夜深了";
+}
+
+//获取时间
+var t = null;
+t = setTimeout(time, 1000);
+
+function time() {
+    clearTimeout(t);
+    dt = new Date();
+    var mm = dt.getMonth() + 1;
+    var d = dt.getDate();
+    var weekday = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
+    var day = dt.getDay();
+    var h = dt.getHours();
+    var m = dt.getMinutes();
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    $("#time_text").html(h + '<span id="point">:</span>' + m);
+    $("#day").html(mm + "&nbsp;月&nbsp;" + d + "&nbsp;日&nbsp;" + weekday[day]);
+    t = setTimeout(time, 1000);
+}
+
+
+//火狐浏览器独立样式
+if (isFirefox = navigator.userAgent.indexOf("Firefox") > 0) {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.href = './css/firefox.css';
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    head.appendChild(link);
+    window.addEventListener('load', function () {
+        setTimeout(function () {
+            iziToast.show({
+                timeout: 8000,
+                message: '您正在使用火狐浏览器，部分功能可能不支持'
+            });
+        }, 3800);
+    }, false)
+}
+
+//Tab书签页
+$(function () {
+    $(".mark .tab .tab-item").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".products .mainCont").eq($(this).index()).css("display", "flex").siblings().css("display", "none");
+    })
+})
+
+//设置
+$(function () {
+    $(".set .tabs .tab-items").click(function () {
+        $(this).addClass("actives").siblings().removeClass("actives");
+        $(".productss .mainConts").eq($(this).index()).css("display", "flex").siblings().css("display", "none");
+    })
+})
+
+//输入框为空时阻止跳转
+$(window).keydown(function (e) {
+    var key = window.event ? e.keyCode : e.which;
+    if (key.toString() == "13") {
+        if ($(".wd").val() == "") {
+            return false;
+        }
+    }
+});
+
