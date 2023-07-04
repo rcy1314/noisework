01b19984c68631a9c7b9fb97d565fb1ab14cca41
NOISE <1129159201@qq.com>
2023-07-05 00:16:26 +0800

    引入Service Worker

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('your-app-name').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/css/main.css',
+          '/js/main.js',
+          // 添加您需要缓存的其他静态资源
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+    
