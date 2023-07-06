cd24b0e156b17947a0a98babbc726bc8d727fd4f
NOISE <1129159201@qq.com>
2023-07-06 12:07:00 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,6 @@ self.addEventListener('install', function(event) {
       caches.open('your-app-name').then(function(cache) {
         return cache.addAll([
           '/',
-          'js/suiji-picture.js',
           '/css/main.css',
           '/js/main.js',
           // 添加您需要缓存的其他静态资源
