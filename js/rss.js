cc95c8944dabd6393a6a48c63d97700caf379197
noise <1129159201@qq.com>
2023-08-21 22:27:22 +0800

    添加私有api key支持的监测刷新

--- js/rss.js
+++ js/rss.js
@@ -1,53 +1,74 @@
-    // JavaScript代码-rss
-    var rssContainer = document.querySelector('.rss-container');
-    var rssItem = document.getElementById('rss-item');
-    var rssSources = [
-      'https://www.noiseblog.top/atom.xml',
-      'https://noisevip.cn/feed',
-      // 添加更多的RSS信息源
-    ];
-    var currentRssIndex = 0;
-    var currentRssItemIndex = 0;
-    
-    function fetchRssItems(url) {
-      fetch('https://api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url))
-        .then(response => response.json())
-        .then(data => {
-          rssItem.innerHTML = ''; // 清空之前的RSS项
-    
-          var rssLink = document.createElement('div');
-          rssLink.classList.add('rss-link');
-          var item = data.items[currentRssItemIndex];
-          var pubDate = new Date(item.pubDate);
-          var formattedDate = pubDate.toLocaleDateString();
-          rssLink.innerHTML = `<a href="${item.link}" target="_blank">${item.title} - ${formattedDate}</a>`;
-    
-          rssItem.appendChild(rssLink);
-    
-          currentRssItemIndex = (currentRssItemIndex + 1) % data.items.length;
-          if (currentRssItemIndex === 0) {
-            currentRssIndex = (currentRssIndex + 1) % rssSources.length;
-          }
-        });
-    }
-    
-    // 获取并解析所有RSS信息源的数据
-    rssSources.forEach(source => {
-      fetchRssItems(source);
+// JavaScript代码-rss
+var rssContainer = document.querySelector('.rss-container');
+var rssItem = document.getElementById('rss-item');
+var rssSources = [
+  'https://www.noiseblog.top/atom.xml',
+  'https://noisevip.cn/feed',
+  // 添加更多的RSS信息源
+];
+var currentRssIndex = 0;
+var currentRssItemIndex = 0;
+var apiKey = 'iaizwlvnlvypvn1qcjnossrguhsckfdsxlqppbur'; // 替换为你的API密钥
+var lastUpdateTimes = {}; // 记录每个RSS源的最后更新时间
+
+function fetchRssItems(url) {
+  fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(url)}&api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      rssItem.innerHTML = ''; // 清空之前的RSS项
+
+      var rssLink = document.createElement('div');
+      rssLink.classList.add('rss-link');
+      var item = data.items[currentRssItemIndex];
+      var pubDate = new Date(item.pubDate);
+      var formattedDate = pubDate.toLocaleDateString();
+      rssLink.innerHTML = `<a href="${item.link}" target="_blank">${item.title} - ${formattedDate}</a>`;
+
+      rssItem.appendChild(rssLink);
+
+      currentRssItemIndex = (currentRssItemIndex + 1) % data.items.length;
+      if (currentRssItemIndex === 0) {
+        currentRssIndex = (currentRssIndex + 1) % rssSources.length;
+      }
     });
-    
-    // 页面载入后延迟2秒后弹出效果
-    setTimeout(function() {
-      rssContainer.classList.add('open');
-    }, 2000);
-    
-    // 点击关闭按钮后隐藏容器
-    var closeButton = document.getElementById('close-button');
-    closeButton.addEventListener('click', function() {
-      rssContainer.style.display = 'none';
-    });
-    
-    // 每隔8秒变换一次信息
-    setInterval(function() {
-      fetchRssItems(rssSources[currentRssIndex]);
-    }, 8000);
+}
+
+// 获取并解析所有RSS信息源的数据
+rssSources.forEach(source => {
+  fetchRssItems(source);
+});
+
+// 页面载入后延迟2秒后弹出效果
+setTimeout(function() {
+  rssContainer.classList.add('open');
+}, 2000);
+
+// 点击关闭按钮后隐藏容器
+var closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', function() {
+  rssContainer.style.display = 'none';
+});
+
+// 每隔8秒变换一次信息
+setInterval(function() {
+  currentRssIndex = (currentRssIndex + 1) % rssSources.length;
+  fetchRssItems(rssSources[currentRssIndex]);
+}, 8000);
+
+// 定时检查RSS源是否有更新
+setInterval(function() {
+  rssSources.forEach(source => {
+    fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(source)}&api_key=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        var latestItem = data.items[0];
+        var pubDate = new Date(latestItem.pubDate);
+        if (!lastUpdateTimes[source] || pubDate > lastUpdateTimes[source]) {
+          // 有新的更新，立即刷新显示最新信息
+          fetchRssItems(source);
+          // 更新最后更新时间
+          lastUpdateTimes[source] = pubDate;
+        }
+      });
+  });
+}, 3600000); // 每隔1小时检查一次RSS源是否有更新
