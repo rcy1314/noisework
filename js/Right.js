93949bb11dd4cc8bb4e582227854dcd32b95a7ab
noise <1129159201@qq.com>
2023-12-29 18:47:00 +0800

    添加右键菜单，修改小样式

---
+++ js/Right.js
@@ -0,0 +1,98 @@
+window.onload = () => {
+    const menu = document.querySelector('.menu')
+    const menuHeight = menu.offsetHeight - parseInt(getComputedStyle(menu)['paddingTop']) - parseInt(getComputedStyle(menu)['paddingBottom'])
+    menu.style.height = '0'
+
+    openMenu = e => {
+        e.preventDefault()
+
+        menu.style.left = `${e.clientX}px`
+        menu.style.top = `${e.clientY + 5}px`
+        menu.style.height = `${menuHeight}px`
+        menu.classList.add('is-active')
+
+        return false
+    }
+
+    colseMenu = () => {
+        menu.style.height = '0'
+        menu.classList.remove('is-active')
+    }
+
+    window.onclick = () => colseMenu()
+}
+let isFullScreen = false;
+
+    function toggleFullScreen() {
+        if (!isFullScreen) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen();
+            }
+            isFullScreen = true;
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+            isFullScreen = false;
+        }
+    }
+
+    function copyContent() {
+        const textToCopy = document.documentElement.innerHTML;
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+                console.log('Text copied to clipboard');
+            })
+            .catch((error) => {
+                console.error('Failed to copy text:', error);
+            });
+    }
+
+    function refreshPage() {
+        location.reload();
+    }
+
+    function goBack() {
+        window.history.back();
+    }
+
+    function changeMode() {
+        // Your code to change the mode goes here
+    }
+    function showImage() {
+        var overlay = document.createElement("div");
+        overlay.style.position = "fixed";
+        overlay.style.top = "0";
+        overlay.style.left = "0";
+        overlay.style.width = "100%";
+        overlay.style.height = "100%";
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        overlay.style.zIndex = "9999";
+        overlay.onclick = function() {
+            document.body.removeChild(overlay);
+        };
+
+        var image = document.createElement("img");
+        image.src = "./lianxi/weixin.png";
+        image.style.position = "absolute";
+        image.style.top = "50%";
+        image.style.left = "50%";
+        image.style.transform = "translate(-50%, -50%)";
+        image.style.maxWidth = "90%";
+        image.style.maxHeight = "90%";
+
+        overlay.appendChild(image);
+        document.body.appendChild(overlay);
+    }
