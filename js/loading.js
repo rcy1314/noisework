5de7a6f8c47c3ad237693a736e8509f688c0d94f
noise <1129159201@qq.com>
2024-05-06 22:15:33 +0800

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', function() {
-    var spinnerContainer = document.getElementById('spinner-container');
+window.addEventListener('DOMContentLoaded', function() {
+  var spinnerContainer = document.getElementById('spinner-container');
+
+  // 设置一个定时器，在5秒后隐藏加载指示器
+  setTimeout(function() {
     spinnerContainer.style.display = 'none';
-  });
-  
+  }, 3000); // 3000毫秒即3秒
+});
