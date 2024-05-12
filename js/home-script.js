e0f7b108dd2e4fcf4516b05f0835bf195fb5e416
noise <1129159201@qq.com>
2024-05-12 19:57:34 +0800

    修改主页home页风格

---
+++ js/home-script.js
@@ -0,0 +1,179 @@
+
+
+function handlePress(event) {
+    this.classList.add('pressed');
+}
+
+function handleRelease(event) {
+    this.classList.remove('pressed');
+}
+
+function handleCancel(event) {
+    this.classList.remove('pressed');
+}
+
+var buttons = document.querySelectorAll('.projectItem');
+buttons.forEach(function (button) {
+    button.addEventListener('mousedown', handlePress);
+    button.addEventListener('mouseup', handleRelease);
+    button.addEventListener('mouseleave', handleCancel);
+    button.addEventListener('touchstart', handlePress);
+    button.addEventListener('touchend', handleRelease);
+    button.addEventListener('touchcancel', handleCancel);
+});
+
+function toggleClass(selector, className) {
+    var elements = document.querySelectorAll(selector);
+    elements.forEach(function (element) {
+        element.classList.toggle(className);
+    });
+}
+
+function pop(imageURL) {
+    var tcMainElement = document.querySelector(".tc-img");
+    if (imageURL) {
+        tcMainElement.src = imageURL;
+    }
+    toggleClass(".tc-main", "active");
+    toggleClass(".tc", "active");
+}
+
+var tc = document.getElementsByClassName('tc');
+var tc_main = document.getElementsByClassName('tc-main');
+tc[0].addEventListener('click', function (event) {
+    pop();
+});
+tc_main[0].addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+
+
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) == 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var html = document.querySelector('html');
+    var themeState = getCookie("themeState") || "Light";
+    var tanChiShe = document.getElementById("tanChiShe");
+
+    function changeTheme(theme) {
+        tanChiShe.src = "../assets/svg/snake-" + theme + ".svg";
+        html.dataset.theme = theme;
+        setCookie("themeState", theme, 365);
+        themeState = theme;
+    }
+
+
+    var Checkbox = document.getElementById('myonoffswitch')
+    Checkbox.addEventListener('change', function () {
+        if (themeState == "Dark") {
+            changeTheme("Light");
+        } else if (themeState == "Light") {
+            changeTheme("Dark");
+        } else {
+            changeTheme("Dark");
+        }
+    });
+
+    if (themeState == "Dark") {
+        Checkbox.checked = false;
+    }
+
+    changeTheme(themeState);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var pageLoading = document.querySelector("#zyyo-loading");
+    setTimeout(function () {
+        pageLoading.style.opacity = '0';
+
+    }, 100);
+
+/*
+    var fpsElement = document.createElement('div');
+    fpsElement.id = 'fps';
+    fpsElement.style.zIndex = '10000';
+    fpsElement.style.position = 'fixed';
+    fpsElement.style.left = '0';
+    document.body.insertBefore(fpsElement, document.body.firstChild);
+
+    var showFPS = (function () {
+        var requestAnimationFrame = window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+
+        var fps = 0,
+            last = Date.now(),
+            offset, step, appendFps;
+
+        step = function () {
+            offset = Date.now() - last;
+            fps += 1;
+
+            if (offset >= 1000) {
+                last += offset;
+                appendFps(fps);
+                fps = 0;
+            }
+
+            requestAnimationFrame(step);
+        };
+
+        appendFps = function (fpsValue) {
+            fpsElement.textContent = 'FPS: ' + fpsValue;
+        };
+
+        step();
+    })();
+    
+    */
+    
+    
+    
+    
+    
+});
+
