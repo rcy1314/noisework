79c6768f4a64c4e86dc90b163dbd270bba7c5f11
rcy1314 <1129159201@qq.com>
2024-08-16 16:57:17 +0800

    完善音效

--- js/sound.js
+++ js/sound.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var hoverSoundlogoDiv = new Audio('../assets/sound/门铃.mp3'); // logoDiv悬停音效
     var hoverSoundavatar = new Audio('../assets/sound/门铃.mp3'); // avatar悬停音效
     var hoverSoundfirst = new Audio('../assets/sound/开瓶.mp3'); // first悬停音效
+    var hoverSoundclock = new Audio('../assets/sound/叮.mp3'); // clock悬停音效
 
     leftClickSound.preload = 'auto';
     rightClickSound.preload = 'auto';
     hoverSoundlogoDiv.preload = 'auto'; // 预加载logoDiv悬停音效
     hoverSoundavatar.preload = 'auto'; // 预加载avatar悬停音效
     hoverSoundfirst.preload = 'auto'; // 预加载first悬停音效
+    hoverSoundclock.preload = 'auto'; // 预加载clock悬停音效
 
     // 尝试播放音频，以确保它们被加载到缓存中
     leftClickSound.load();
@@ -18,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     hoverSoundlogoDiv.load(); // 加载logoDiv悬停音效
     hoverSoundavatar.load(); // 加载avatar悬停音效
     hoverSoundfirst.load(); // 加载first悬停音效
+    hoverSoundclock.load(); // 加载clock悬停音效
 
     // 鼠标点击音效函数
     function playSound(audioObject) {
@@ -25,12 +28,14 @@ document.addEventListener('DOMContentLoaded', function() {
         audioObject.play();
     }
 
-    // 检测屏幕宽度，如果是手机尺寸，则不播放鼠标点击音效
-    var isMobile = window.matchMedia("screen and (max-width: 768px)").matches;
+    // 检测设备类型
+    function isMobileDevice() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
 
     // 监听鼠标点击事件
     document.addEventListener('mousedown', function(event) {
-        if (!isMobile) { // 如果不是手机尺寸
+        if (!isMobileDevice()) { // 如果不是移动设备
             if (event.button === 0) { // 左键点击
                 playSound(leftClickSound); // 播放左键点击音效
             } else if (event.button === 2) { // 右键点击
@@ -71,4 +76,16 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.warn('没有找到ID为avatar的元素');
     }
+
+    // 获取logoDiv元素
+    var clock = document.getElementById('clock');
+    if (clock) {
+        // 监听鼠标悬停事件
+        clock.addEventListener('mouseover', function() {
+            playSound(hoverSoundclock); // 播放clock悬停音效
+        });
+    } else {
+        console.warn('没有找到ID为clock的元素');
+    }
+
 });
