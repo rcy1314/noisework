5b13be8a8142f9178b7270b7372e5feddbafb680
rcy1314 <1129159201@qq.com>
2024-09-05 17:16:54 +0800

    修改组件样式

--- js/tab.js
+++ js/tab.js
@@ -1,8 +1,32 @@
-// 要操作的元素
-const menu_box=document.querySelector('.menu-box');
-const menu_button=document.querySelector('.menu-button');
+const menu_box = document.querySelector('.menu-box');
+const menu_button = document.querySelector('.menu-button');
 
-// 为菜单按钮绑定点击事件
-menu_button.addEventListener('click',function(){
+menu_button.addEventListener('click', function() {
     menu_box.classList.toggle('active');
-})
+});
+
+let isDragging = false;
+let startX, startY, initialX, initialY;
+
+menu_box.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startX = e.clientX;
+    startY = e.clientY;
+    initialX = menu_box.offsetLeft;
+    initialY = menu_box.offsetTop;
+    menu_box.style.cursor = 'grabbing';
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        const dx = e.clientX - startX;
+        const dy = e.clientY - startY;
+        menu_box.style.left = `${initialX + dx}px`;
+        menu_box.style.top = `${initialY + dy}px`;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+    menu_box.style.cursor = 'grab';
+});
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'Noise主页-v2.3.2';
+var cacheName = 'Noise主页-v2.4';
 var assetsToCache = [
   './home.html',
   './index.html',
