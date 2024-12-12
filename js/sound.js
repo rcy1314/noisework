f7f0a8ace9e7bf5a21e54998b58749e3ac8d32d7
rcy1314 <1129159201@qq.com>
2024-12-12 18:42:26 +0800

    添加热榜组件音效

--- js/sound.js
+++ js/sound.js
@@ -174,7 +174,7 @@ function addSoundToElements(selectors, sound) {
 }
 
 // 网址过渡音数组
-var elementsSelectors = ['.iconItem', '.item', '.projectItem','.project','.switchbg','.mobileswitchbg','.friendlinks-card','.bilibilititle-left','.bilibilititle-right','.menu-item','.menu-list li'];
+var elementsSelectors = ['.iconItem', '.item', '.projectItem','.project','.switchbg','.mobileswitchbg','.friendlinks-card','.bilibilititle-left','.bilibilititle-right','.menu-item','.hotcard','.menu-list li'];
 addSoundToElements(elementsSelectors, websiteHoverSound);
 
 // 广告位元素选择器数组
